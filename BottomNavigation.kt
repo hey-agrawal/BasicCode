5fa7943f5fb6cbce2f66f7cb7e5b53209434ae0c
Prasoon Agrawal <115768837+hey-agrawal@users.noreply.github.com>
2023-07-10 10:57:41 +0530

    Update BottomNavigation.kt

--- BottomNavigation.kt
+++ BottomNavigation.kt
@@ -20,7 +20,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
-import com.example.appcompose.hyperlink.HyperlinkText
 
 @Composable
 fun BottomNavigation(){
