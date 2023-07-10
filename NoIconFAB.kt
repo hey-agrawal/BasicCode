1aabc48faced2f4929f92cda071fd03431d0b815
Prasoon Agrawal <115768837+hey-agrawal@users.noreply.github.com>
2023-07-10 11:39:20 +0530

    Create NoIconFAB.kt

---
+++ NoIconFAB.kt
@@ -0,0 +1,24 @@
+package com.example.appcompose.screens.floatingActionsButtons
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.ExtendedFloatingActionButton
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+
+@Composable
+fun SimpleExtendedFabNoIcon(){
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()) {
+        ExtendedFloatingActionButton(
+            text = { Text("No Icon") },
+            onClick = {}
+        )
+    }
+}
