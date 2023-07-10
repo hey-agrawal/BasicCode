67be0d1b324e20c427018418853c4a0d9b94df96
Prasoon Agrawal <115768837+hey-agrawal@users.noreply.github.com>
2023-07-10 17:06:59 +0530

    Create ScrollingTextTab.kt

---
+++ ScrollingTextTab.kt
@@ -0,0 +1,77 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.compose.animation.animateColor
+import androidx.compose.animation.core.animateDp
+import androidx.compose.animation.core.spring
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.LeadingIconTab
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.ScrollableTabRow
+import androidx.compose.material.Tab
+import androidx.compose.material.TabPosition
+import androidx.compose.material.TabRow
+import androidx.compose.material.TabRowDefaults.tabIndicatorOffset
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+import androidx.compose.foundation.layout.Column
+
+@Composable
+fun ScrollingTextTabs(){
+    Column() {
+        var state by remember { mutableStateOf(0) }
+        val titles = listOf(
+            "TAB 1",
+            "TAB 2",
+            "TAB 3 WITH LOTS OF TEXT",
+            "TAB 4",
+            "TAB 5",
+            "TAB 6 WITH LOTS OF TEXT",
+            "TAB 7",
+            "TAB 8",
+            "TAB 9 WITH LOTS OF TEXT",
+            "TAB 10"
+        )
+        Column {
+            ScrollableTabRow(selectedTabIndex = state) {
+                titles.forEachIndexed { index, title ->
+                    Tab(
+                        text = { Text(title) },
+                        selected = state == index,
+                        onClick = { state = index }
+                    )
+                }
+            }
+            Text(
+                modifier = Modifier.align(Alignment.CenterHorizontally),
+                text = "Scrolling text tab ${state + 1} selected",
+                style = MaterialTheme.typography.body1
+            )
+        }
+    }
+}
