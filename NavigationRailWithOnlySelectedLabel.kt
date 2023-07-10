0460005154b1d338018a0d67bc8ae7bf99a2016e
Prasoon Agrawal <115768837+hey-agrawal@users.noreply.github.com>
2023-07-10 11:05:31 +0530

    Create NavigationRailWithOnlySelectedLabel.kt

---
+++ NavigationRailWithOnlySelectedLabel.kt
@@ -0,0 +1,44 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.navigation.NavHostController
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Icon
+import androidx.compose.material.NavigationRail
+import androidx.compose.material.NavigationRailItem
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.filled.Settings
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+
+@Composable
+fun NavigationRailWithOnlySelectedLabels(){
+    Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center,
+        modifier = Modifier
+            .fillMaxSize()) {
+        var selectedItem by remember { mutableStateOf(0) }
+        val items = listOf("Menu", "Search", "Settings")
+        val icons = listOf(Icons.Filled.Menu, Icons.Filled.Search, Icons.Filled.Settings)
+        NavigationRail {
+            items.forEachIndexed { index, item ->
+                NavigationRailItem(
+                    icon = { Icon(icons[index], contentDescription = item) },
+                    label = { Text(item) },
+                    selected = selectedItem == index,
+                    onClick = { selectedItem = index },
+                    alwaysShowLabel = false
+                )
+            }
+        }
+    }
+}
