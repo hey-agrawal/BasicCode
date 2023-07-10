13a0773b0f75b02763ce8c38078ad6cbc9d98898
Prasoon Agrawal <115768837+hey-agrawal@users.noreply.github.com>
2023-07-10 14:15:44 +0530

    Create Sliders.kt

---
+++ Sliders.kt
@@ -0,0 +1,20 @@
+import androidx.compose.material.Slider
+import androidx.compose.material.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+
+@Composable
+fun SliderSample(){
+    Column() {
+        var sliderPosition by remember { mutableStateOf(0f) }
+        Text(text = sliderPosition.toString())
+        Slider(value = sliderPosition, onValueChange = { sliderPosition = it })
+    }
+}
